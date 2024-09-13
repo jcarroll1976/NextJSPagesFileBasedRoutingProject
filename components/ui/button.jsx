ac78305c8750fded8c4a1b579326740cbdf09977
Joshua Carroll <carrolljosh1976@gmail.com>
2024-09-12 21:51:00 -0400

    added Button,AddressIcon,ArrowRightIcon, and DateIcon components, implemented them in EventItem component

---
+++ components/ui/button.jsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+import styles from "./button.module.css";
+
+export default function Button(props) {
+    return (
+        <Link href={props.link}className={styles.btn}>{props.children}</Link>
+    )
+}

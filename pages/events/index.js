113f396e6dc7f9421ecfb2f7f4c2a0e7126719c5
Joshua Carroll <carrolljosh1976@gmail.com>
2024-09-17 22:04:11 -0400

    added submitHandler function inside EventsSearch component and findEventsHandler function inside EventsPage to navigate to filtered events page displaying filtered events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,12 +1,21 @@
+import { useRouter } from "next/router";
+
 import EventsList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data"
 
 export default function EventsPage() {
     const events = getAllEvents();
+    const router = useRouter();
+
+    function findEventsHandler(year,month) {
+        const fullPath = `/events/${year}/${month}`
+
+        router.push(fullPath);
+    }
     return (
         <div>
-            <EventsSearch />
+            <EventsSearch onSearch={findEventsHandler} />
             <EventsList items={events} />
         </div>
        

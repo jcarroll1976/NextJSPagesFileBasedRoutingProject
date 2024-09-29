eb6896e835f7546013c0c45b8df35877be9f88ca
Joshua Carroll <carrolljosh1976@gmail.com>
2024-09-29 13:58:49 -0400

    Added ErrorAlert component to EventDetailPage

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -5,6 +5,7 @@ import { Fragment } from "react";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
+import ErrorAlert from "../../components/ui/error-alert/error-alert";
 
 export default function EventDetailPage() {
     const router = useRouter();
@@ -14,7 +15,7 @@ export default function EventDetailPage() {
     const event = getEventById(eventId);
 
     if(!event) {
-        return <p>No event found!</p>;
+        return <ErrorAlert><p>No event found!</p></ErrorAlert>;
     }
 
     return (

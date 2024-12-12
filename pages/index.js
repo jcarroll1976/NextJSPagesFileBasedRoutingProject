692d5c10f9daed882e2f210a834be4a4a7800099
Joshua Carroll <carrolljosh1976@gmail.com>
2024-12-12 04:28:02 -0500

    added modified getEventById function to api-util.js to get event by its id from Firebase

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import EventsList from "../components/events/event-list";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-export default function HomePage() {
-    const featuredEvents = getFeaturedEvents();
+export default function HomePage(props) {
     return (
         <div>
-            <EventsList items={featuredEvents} />
+            <EventsList items={props.events} />
         </div>
         
     )
+}
+
+export async function getStaticProps() {
+    const featuredEvents = await getFeaturedEvents();
+    return {
+        props: {
+            events: featuredEvents,
+        }
+    }
 }
